c9fc5b362c50f71e5beac257b2ac7327d752ba22
Ryan Levick <ryan.levick@gmail.com>
2019-10-25 15:13:54 +0200

    Update to use latest reqwest and async/await

--- examples/actix-state/service.rs
+++ examples/actix-state/service.rs
@@ -4,18 +4,21 @@ use daprs::state::StateClient;
 use http::StatusCode;
 use log::error;
 
-pub fn post(json_order: Json<Order>, state_client: Data<StateClient>) -> HttpResponse {
+pub async fn post(json_order: Json<Order>, state_client: Data<StateClient>) -> HttpResponse {
     let order = json_order.into_inner();
-    if let Err(e) = state_client.save("order", order.clone()) { 
-        error!("Error pushing state for key: order value: {:?}, received: {}", order, e);
+    if let Err(e) = state_client.save("order", order.clone()).await {
+        error!(
+            "Error pushing state for key: order value: {:?}, received: {}",
+            order, e
+        );
         ErrorResponse::from(e).into()
     } else {
         HttpResponse::build(StatusCode::ACCEPTED).finish()
     }
 }
 
-pub fn get(state_client: Data<StateClient>) -> HttpResponse {
-    match state_client.get::<Order>("order") {
+pub async fn get(state_client: Data<StateClient>) -> HttpResponse {
+    match state_client.get::<Order>("order").await {
         Ok(order) => HttpResponse::build(StatusCode::OK).json(order),
         Err(e) => {
             error!("Error retrieving state for key: order, received {}", e);
